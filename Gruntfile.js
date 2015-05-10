01961e53238800d07cbb4626c23c3d6170e3c248
UPGLE <admin@upgle.com>
2015-05-10 19:06:18 +0900

    for test

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+
+module.exports = function(grunt) {
+
+grunt.initConfig({
+ 
+  'gh-pages': {
+    options: {
+      base: 'dist'
+    },
+    src: ['**/*']
+  }
+
+
+});
+grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask('default', ['gh-pages']);
+
+};
+
+
